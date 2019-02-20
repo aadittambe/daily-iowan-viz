cb4e093499ef99ddf91173303918ad8231ca7602
aadittambe <46935306+aadittambe@users.noreply.github.com>
2019-02-20 17:33:33 -0600

    Update tracker.js

--- candidate-tracker/tracker.js
+++ candidate-tracker/tracker.js
@@ -2,7 +2,7 @@ var mymap = L.map('mapid').setView([41.6611, -91.5302], 25);
 
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
+    maxZoom: 100,
     id: 'mapbox.streets',
     accessToken: 'pk.eyJ1IjoiYWFkaXR0YW1iZSIsImEiOiJjanNkcGMxbmowMG5lNDRydDd3Mm5taHVjIn0.fP9wK52zPEVxVfVau5bQOQ'
 }).addTo(mymap);
